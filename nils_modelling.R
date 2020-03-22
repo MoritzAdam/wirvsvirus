9e84818ebbd9e991e019ef6292eff97896516197
Nils Weitzel <nweitzel@iup.uni-heidelberg.de>
2020-03-22 18:16:34 +0100

    Mit glasso

--- nils_modelling.R
+++ nils_modelling.R
@@ -83,17 +83,14 @@ points(exp_coef[bundeslaender$rev_id],col="red")
 # Use Day of week as predictor (or seven-day weighted mean)? --> Weighted mean würde daten deutlich smoothen
 
 library(bfast)
-for (i in 2:17) {
-  tmp <- bfast(ts(log(rki_bundesland_cum[i,which(rki_bundesland_cum[i,]>0)]),frequency = 1),h=0.4,season="none",max.iter = 10)
-  plot(tmp,main=bundeslaender$name[i])
+breakpoints <- list()
+breakpoints_num <- 0
+for (i in 1:16) {
+  breakpoints[[i]] <- bfast(ts(log(rki_bundesland_cum[i,which(rki_bundesland_cum[i,1:51]>0)]),frequency = 1),h=0.2,season="none",max.iter = 10)
+  breakpoints_num[i] <- breakpoints[[i]]$Time
+  plot(breakpoints[[i]],main=bundeslaender$name[i])
 }
-
-
-library(glmnet)
-tmp_cv <- cv.glmnet(as.matrix(predictors),rki_bundesland_cum[,18])
-tmp_best_lamda <- tmp_cv$lambda.min
-tmp_lasso <- glmnet(as.matrix(predictors),rki_bundesland_cum[,18],alpha = 1, lambda = tmp_best_lamda)
-emulator$sel_predictors[[i]] <- which(as.matrix(tmp_lasso$beta) != 0)
+breakpoints_num <- sapply(breakpoints_num,function(x) max(1,x,na.rm=T))
 
 predictors_laender <- read.csv("predictors_laenderebene.csv",sep="\t")
 predictors_laender$Altenquotient.2018
@@ -117,19 +114,90 @@ anti_join(fallzahlen %>% filter(IdBundesland > 0) %>% mutate_at(vars(IdLandkreis
 # Dataframe LK / BL
 # ID, name, aktuelle fallzahl, wachstumrate (im prinzip aus den daten, über die du den loess smoother gelegt hast), prediktoren
 
-regression_data_land <- data.frame(
-  id <- bundeslaender$ID,
-  shortname <- predictors_laender$Land[reorder_laender],
-  fallzahl <- rki_bundesland_cum[,51],
-  altenquotient <- as.numeric(predictors_laender$Altenquotient.2018)[reorder_laender],
-  intensivbetten <- as.numeric(predictors_laender$Anzahl.Intensivbetten..Tsd..Einwohner)[reorder_laender],
-  dichte <- as.numeric(predictors_laender$Bevölerungsdichte.1.km.2)[reorder_laender]
-)
+wachstum_coeff <- 0
+for (i in 1:16) {
+  test_lm <- lm(log(y) ~ x,data=data.frame(y=rki_bundesland_cum[i,which(rki_bundesland_cum[i,1:51]>0)[-(1:breakpoints_num[i])]],x=relevant_dates[which(rki_bundesland_cum[i,1:51]>0)[-(1:breakpoints_num[i])]]))
+  wachstum_coeff[i] <- test_lm$coefficients[2]
+}
 
 predictors_laender <- read.csv("predictors_laenderebene.csv",colClasses = "character",sep="\t")
 predictors_landkreise <- read.csv("predictors_landkreise_mit_mobilitaet.csv",colClasses = "character")
-
 reorder_laender <- c(1,13,3,4,2,12,11,6,7,5,10,14,15,16,9,8)
+regression_data_land <- data.frame(
+  id = bundeslaender$ID[reorder_laender],
+  shortname = as.character(predictors_laender$Land),
+  langname = as.character(bundeslaender$name)[reorder_laender],
+  fallzahl = rki_bundesland_cum[reorder_laender,51]/as.numeric(predictors_laender$Einwohner.in.Tsd...Stand.31..Dez..2018.)*100,
+  wachstum = wachstum_coeff[reorder_laender],
+  altenquotient = as.numeric(predictors_laender$Altenquotient.2018),
+  intensivbetten = as.numeric(predictors_laender$Anzahl.Intensivbetten..Tsd..Einwohner),
+  dichte = as.numeric(predictors_laender$Bevölerungsdichte.1.km.2)
+)
+
+library(glmnet)
+
+tmp_cv <- cv.glmnet(as.matrix(regression_data_land[,6:8]),regression_data_land$fallzahl)
+tmp_best_lamda <- tmp_cv$lambda.min
+tmp_lasso <- glmnet(as.matrix(regression_data_land[,6:8]),regression_data_land$fallzahl,alpha = 1, lambda = tmp_best_lamda)
+sel_predictors <- which(as.matrix(tmp_lasso$beta) != 0)
+plot(regression_data_land$fallzahl,pch=20,col="red")
+points(predict(tmp_cv,newx = as.matrix(regression_data_land[,6:8])),pch=20,col="blue")
+plot(regression_data_land$altenquotient,regression_data_land$fallzahl,pch=20)
+cor(regression_data_land$altenquotient,regression_data_land$fallzahl)
+tmp_lasso$beta
+
+tmp_cv <- cv.glmnet(as.matrix(regression_data_land[,6:8]),regression_data_land$wachstum)
+tmp_best_lamda <- tmp_cv$lambda.min
+tmp_lasso <- glmnet(as.matrix(regression_data_land[,6:8]),regression_data_land$wachstum,alpha = 1, lambda = tmp_best_lamda)
+sel_predictors <- which(as.matrix(tmp_lasso$beta) != 0)
+plot(regression_data_land$wachstum,pch=20,col="red")
+points(predict(tmp_cv,newx = as.matrix(regression_data_land[,6:8])),pch=20,col="blue")
+plot(regression_data_land$altenquotient,regression_data_land$wachstum,pch=20)
+cor(regression_data_land$altenquotient,regression_data_land$wachstum)
+tmp_lasso$beta
 
 
+# [1] "Landkreis"                               "Meldedatum"                              "IdLandkreis"                            
+# [4] "sum_LK"                                  "csum"                                    "Wert"                                   
+# [7] "Fläche"                                  "Bevölkerung(2018)"                       "männlich"                               
+# [10] "weiblich"                                "Anteil Alter0-17 in (%)"                 "Anteil Alter18-24 in (%)"               
+# [13] "Anteil Alter25-44 in (%)"                "Anteil Alter25-64 in (%)"                "Anteil Alter65-alt in (%)"              
+# [16] "Krankenhausbetten/1000 Einwohner"        "Einkommen"                               "Raumtyp"                                
+# [19] "Regiontyp"                               "Raumtyp_Name"                            "Personenkilometer pro Einwohner pro Tag"
+# [22] "Anteil unter 3 Wege pro Tag"             "Anteil 3 oder mehr Wege pro Tag"         "csum_LK_pro_1kEinwohner"                
+# [25] "gr"                 
+predictors <- c(8:18,21:23)
+tmp_cv <- cv.glmnet(as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors])),as.numeric(LK_dat_proc_filtered$csum_LK_pro_1kEinwohner*100))
+tmp_best_lamda <- tmp_cv$lambda.min
+tmp_lasso <- glmnet(as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors])),as.numeric(LK_dat_proc_filtered$csum_LK_pro_1kEinwohner*100),alpha = 1, lambda = tmp_best_lamda)
+sel_predictors <- which(as.matrix(tmp_lasso$beta) != 0)
+plot(as.numeric(LK_dat_proc_filtered$csum_LK_pro_1kEinwohner*100),pch=20,col="red")
+points(predict(tmp_lasso,newx = as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors]))),pch=20,col="blue")
+#plot(regression_data_land$altenquotient,regression_data_land$fallzahl,pch=20)
+#cor(regression_data_land$altenquotient,regression_data_land$fallzahl)
+cor(as.numeric(LK_dat_proc_filtered$csum_LK_pro_1kEinwohner*100),predict(tmp_lasso,newx = as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors]))))
+tmp_lasso$beta
 
+predictors <- c(8:18,21:23)
+ref_data <- LK_dat_proc_filtered$csum_LK_pro_1kEinwohner*100
+ref_data <- LK_dat_proc_filtered$gr
+tmp_cv <- cv.glmnet(as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors])),as.numeric(ref_data))
+tmp_best_lamda <- tmp_cv$lambda.min
+tmp_lasso <- glmnet(as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors])),as.numeric(ref_data),alpha = 1, lambda = tmp_best_lamda)
+sel_predictors <- which(as.matrix(tmp_lasso$beta) != 0)
+plot(as.numeric(ref_data),pch=20,col="red")
+points(predict(tmp_lasso,newx = as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors]))),pch=20,col="blue")
+#plot(regression_data_land$altenquotient,regression_data_land$fallzahl,pch=20)
+#cor(regression_data_land$altenquotient,regression_data_land$fallzahl)
+cor(as.numeric(ref_data),predict(tmp_lasso,newx = as.matrix(as.data.frame(LK_dat_proc_filtered[,predictors]))))
+tmp_lasso$beta
+cor(as.numeric(ref_data),as.numeric(LK_dat_proc_filtered$Einkommen))
+
+cor(as.numeric(ref_data),as.numeric(LK_dat_proc_filtered$`Anteil Alter18-24 in (%)`))
+cor(as.numeric(ref_data),as.numeric(LK_dat_proc_filtered$`Anteil Alter65-alt in (%)`))
+cor(as.numeric(ref_data),as.numeric(LK_dat_proc_filtered$Einkommen))
+
+sapply(c(8:18,21:23),function(x) cor(as.numeric(ref_data),c(as.matrix(as.data.frame(LK_dat_proc_filtered[,x])))))
+
+plot(c(as.matrix(as.data.frame(LK_dat_proc_filtered[,8]))),as.numeric(ref_data))
+plot(c(as.matrix(as.data.frame(LK_dat_proc_filtered[,17]))),as.numeric(ref_data))
