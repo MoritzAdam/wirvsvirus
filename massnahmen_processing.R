1696d6b9f160ee488f22ee9556d5d9872797798b
bellerho <beatrice.ellerhoff@iup.uni-heidelberg.de>
2020-04-09 14:03:41 +0200

    auth token with googledrive

--- massnahmen_processing.R
+++ massnahmen_processing.R
@@ -1,13 +1,29 @@
-library("googlesheets4")
-library("tidyverse")
+library(googlesheets4)
+library(googledrive)
+library(tidyverse)
+library(readr)
+#drive_auth()
+#sheets_auth(token = drive_token())
 
-sheets_deauth() 
-test <- read_sheet("https://docs.google.com/spreadsheets/d/1J8KhzH5UwtVdXEViRASh1SVCpJSbicCpLb6Bedvf4JA/edit?usp=sharing")
+if(drive_has_token()==FALSE){
+  print("No authentification with googledrive set up so far, please use drive_auth() and sheets_auth(token = drive_token())")
+}
 
-grepl("Postleitzahl|Welche Maßname", names(test))
+if (drive_has_token()){
 
-myVectorOfStrings <- c("Wann wurden die Maßnahmen", "Postleitzahl", "Um welche Maßnahme")
-matchExpression <- paste(myVectorOfStrings, collapse = "|")
-test2 <- test %>% select(matches(matchExpression)) %>% rename(wann=1, plz=2, was=3)
+setwd("/home/bea/Documents/WirVsVirus") 
 
-                         
+response <- read_sheet("https://docs.google.com/spreadsheets/d/1J8KhzH5UwtVdXEViRASh1SVCpJSbicCpLb6Bedvf4JA/edit?usp=sharing") %>% 
+  select(matches("Wann wurden die Maßnahmen|Postleitzahl|Um welche Maßnahme|wieder aufgehoben|Wann wurde die Maßnahme aufgehoben")) %>% rename(wann=1, plz=2, was=3, aufgehoben=4, wann_aufgehoben=5)
+
+gverzeichnis.lookup <- read_delim(paste0(getwd(), "/wirvsvirus/data_landkreise/gemeindeverzeichnis.csv"), ";", escape_double = FALSE, trim_ws = TRUE)  %>% 
+  select(matches("Amtl.Gemeindeschlüssel|PLZ Ort")) %>% rename(schlüssel=1, plz_ort=2)
+
+gverzeichnis <- tibble(
+  plz = as.numeric(unlist(regmatches(gverzeichnis.lookup$plz_ort, gregexpr("[[:digit:]]+", gverzeichnis.lookup$plz_ort)))),
+  IdLandkreis = gverzeichnis.lookup$schlüssel %>% substr(1, 5)
+)
+
+response %>% left_join(gverzeichnis)
+
+}
