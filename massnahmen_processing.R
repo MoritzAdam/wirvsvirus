79c5bc66a81a7ab54103d4d030ef755fa7073931
bellerho <beatrice.ellerhoff@iup.uni-heidelberg.de>
2020-04-10 19:21:18 +0200

    update zusätzlich stadt

--- massnahmen_processing.R
+++ massnahmen_processing.R
@@ -12,20 +12,22 @@ if(drive_has_token()==FALSE){
 
 if (drive_has_token()){
 
-setwd("/home/bea/Documents/WirVsVirus") 
-
-response <- read_sheet("https://docs.google.com/spreadsheets/d/1J8KhzH5UwtVdXEViRASh1SVCpJSbicCpLb6Bedvf4JA/edit?usp=sharing") %>% 
-  select(matches("Wann wurden die Maßnahmen|Postleitzahl|Um welche Maßnahme|Erzähle uns mehr|wieder aufgehoben|Wann wurde die Maßnahme aufgehoben")) %>% rename(wann=1, plz=2, was=3, info=4, aufgehoben=5, wann_aufgehoben=6)
-
-gverzeichnis.lookup <- read_delim(paste0(getwd(), "/wirvsvirus/data_landkreise/gemeindeverzeichnis.csv"), ";", escape_double = FALSE, trim_ws = TRUE)  %>% 
-  select(matches("Amtl.Gemeindeschlüssel|PLZ Ort")) %>% rename(schlüssel=1, plz_ort=2)
-
-gverzeichnis <- tibble(
-  plz = as.numeric(unlist(regmatches(gverzeichnis.lookup$plz_ort, gregexpr("[[:digit:]]+", gverzeichnis.lookup$plz_ort)))),
-  IdLandkreis = gverzeichnis.lookup$schlüssel %>% substr(1, 5)
-)
-
-response %>% left_join(gverzeichnis) %>% filter(was != "sonstiges")
+  setwd("/home/bea/Documents/WirVsVirus") 
+  
+  response <- read_sheet("https://docs.google.com/spreadsheets/d/1J8KhzH5UwtVdXEViRASh1SVCpJSbicCpLb6Bedvf4JA/edit?usp=sharing") %>% 
+    select(matches("Wann wurden die Maßnahmen|Postleitzahl|Stadt|Um welche Maßnahme|Erzähle uns mehr|wieder aufgehoben|Wann wurde die Maßnahme aufgehoben")) %>% rename(wann=1, plz=2, stadt=3, was=4, info=5, aufgehoben=6, wann_aufgehoben=7)
+  
+  gverzeichnis.lookup <- read_delim(paste0(getwd(), "/wirvsvirus/data_landkreise/gemeindeverzeichnis.csv"), ";", escape_double = FALSE, trim_ws = TRUE)  %>% 
+    select(matches("Amtl.Gemeindeschlüssel|PLZ Ort")) %>% rename(schlüssel=1, plz_ort=2)
+  
+  gverzeichnis <- tibble(
+    plz = as.numeric(unlist(regmatches(gverzeichnis.lookup$plz_ort, gregexpr("[[:digit:]]+", gverzeichnis.lookup$plz_ort)))),
+    IdLandkreis = gverzeichnis.lookup$schlüssel %>% substr(1, 5)
+  )
+  
+  sonstige_massnahmen <- response %>% filter(was == "sonstiges")
+  
+  response %>% as_tibble() %>% left_join(gverzeichnis) %>% filter(was != "sonstiges") %>% filter(!is.na(IdLandkreis))
 
 }
 
